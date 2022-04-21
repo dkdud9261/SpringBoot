b8c9587d28e1cefdf7968faeb1c71adb0358f7cb
dkdud9261 <ayoganatanatda@naver.com>
2022-04-21 23:53:21 +0900

    board project2

--- board/src/main/java/org/zerock/board/repository/ReplyRepository.java
+++ board/src/main/java/org/zerock/board/repository/ReplyRepository.java
@@ -3,12 +3,15 @@ package org.zerock.board.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.zerock.board.entity.Board;
 import org.zerock.board.entity.Reply;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
+
     @Modifying
     @Query(value = "delete from Reply r where r.board.bno =:bno")
     void deleteByBno(Long bno);
--- board/src/main/java/org/zerock/board/service/ReplyServiceImpl.java
+++ board/src/main/java/org/zerock/board/service/ReplyServiceImpl.java
@@ -38,6 +38,6 @@ public class ReplyServiceImpl implements ReplyService {
 
     @Override
     public void remove(Long rno) {
-        replyRepository.deleteByBno(rno);
+        replyRepository.deleteById(rno);
     }
 }
