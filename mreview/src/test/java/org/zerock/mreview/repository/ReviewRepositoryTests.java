d51d6b7728addd344cf62981a30eb68b34de6da3
dkdud9261 <ayoganatanatda@naver.com>
2022-04-22 23:58:51 +0900

    mreview project1

---
+++ mreview/src/test/java/org/zerock/mreview/repository/ReviewRepositoryTests.java
@@ -0,0 +1,43 @@
+package org.zerock.mreview.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.mreview.entity.Member;
+import org.zerock.mreview.entity.Movie;
+import org.zerock.mreview.entity.Review;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class ReviewRepositoryTests {
+
+    @Autowired
+    ReviewRepository reviewRepository;
+
+    @Test
+    public void insertMovieReviews() {
+        IntStream.rangeClosed(1,200).forEach(i -> {
+            Long mno = (long)(Math.random()*100)+1;
+            Long mid = (long)(Math.random()*100)+1;
+            Member member = Member.builder().mid(mid).build();
+            Review movieReview = Review.builder()
+                    .member(member)
+                    .movie(Movie.builder().mno(mno).build())
+                    .grade((int)(Math.random()*5)+1)
+                    .text("이 영화에 대한 느낌..."+i)
+                    .build();
+            reviewRepository.save(movieReview);
+        });
+    }
+
+    @Test
+    public void testGetMovieReviews() {
+        Movie movie = Movie.builder().mno(94L).build();
+        List<Review> result = reviewRepository.findByMovie(movie);
+        result.forEach(movieReview -> {
+            System.out.println(movieReview.getReviewnum()+"\t"+movieReview.getGrade()+"\t"+movieReview.getText()+"\t"+movieReview.getMember().getEmail()+"----------------------------");
+        });
+    }
+}
