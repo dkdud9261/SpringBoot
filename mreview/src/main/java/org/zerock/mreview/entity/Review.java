c5434693d634d36c998df0b5640f3e11ba3ada14
dkdud9261 <ayoganatanatda@naver.com>
2022-05-19 22:06:03 +0900

    mreview project4

--- mreview/src/main/java/org/zerock/mreview/entity/Review.java
+++ mreview/src/main/java/org/zerock/mreview/entity/Review.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Getter
 @ToString(exclude = {"movie", "member"})
-public class Review {
+public class Review extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,4 +25,12 @@ public class Review {
     private int grade;
 
     private String text;
+
+    public void changeGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public void changeText(String text) {
+        this.text = text;
+    }
 }
