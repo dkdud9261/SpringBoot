5228e0e65015a24be9dae91905e20c08dc7dca21
황아영 <ayoganatanatda@naver.com>
2022-07-29 19:25:08 +0900

    jpa uses - develop entity classes2

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -8,6 +8,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
 @Table(name = "orders")
 @Getter @Setter
@@ -18,14 +20,14 @@ public class Order {
     private Long id;
 
     // 연관관계 주됨
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
-    private List<OrderItem> orderItems = new ArrayList<>();
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)   // CascadeType.ALL : Order를 persist하면 orderItems의 원소도 다 persist(전파)
+    private List<OrderItem> orderItems = new ArrayList<>();     //                  원래는 OrderItem 먼저 일일이 persist하고 나서 Order를 persist
 
-    @OneToOne
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -33,5 +35,22 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
+
+    //==연관관계 메서드==//
+    // 핵심적으로 컨트롤하는 쪽이 가지고 있는 것이 좋다.
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        this.orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 
 }
